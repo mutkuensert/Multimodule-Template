31fdd7e74c687d2f97d2d4dff6170de051856097
M. Utku Ensert <mutkuensert@hotmail.com>
2025-03-08 13:02:12 +0300

    fix: change status bar color to transparent

--- core/ui/src/main/java/core/ui/Theme.kt
+++ core/ui/src/main/java/core/ui/Theme.kt
@@ -1,7 +1,7 @@
 package core.ui
 
-import android.app.Activity
 import android.os.Build
+import androidx.activity.compose.LocalActivity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
@@ -10,10 +10,8 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
-import androidx.core.view.ViewCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -49,14 +47,15 @@ fun MyApplicationTheme(
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
+
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
     val view = LocalView.current
     if (!view.isInEditMode) {
+        val activity = LocalActivity.current
         SideEffect {
-            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
-            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
+            activity?.getInsetsController()?.isAppearanceLightStatusBars = !darkTheme
         }
     }
 
--- feature/movies/presentation/src/main/kotlin/feature/movies/presentation/MoviesScreen.kt
+++ feature/movies/presentation/src/main/kotlin/feature/movies/presentation/MoviesScreen.kt
@@ -1,20 +1,16 @@
 package feature.movies.presentation
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.windowInsetsTopHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
@@ -37,24 +33,14 @@ fun MoviesScreen(
 ) {
     val popularMovies = viewModel.popularMovies.collectAsLazyPagingItems()
 
-    Box(modifier = Modifier.fillMaxSize()) {
-        PopularMovies(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 10.dp),
-            popularMovies = popularMovies,
-            navigateToMovieDetails = {},
-            onAddToFavorite = { isFavorite, movieId -> }
-        )
-
-        Spacer(
-            Modifier
-                .fillMaxWidth()
-                .background(MaterialTheme.colorScheme.surface)
-                .windowInsetsTopHeight(WindowInsets.statusBars)
-                .align(Alignment.TopCenter)
-        )
-    }
+    PopularMovies(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 10.dp),
+        popularMovies = popularMovies,
+        navigateToMovieDetails = {},
+        onAddToFavorite = { isFavorite, movieId -> }
+    )
 }
 
 @Composable
