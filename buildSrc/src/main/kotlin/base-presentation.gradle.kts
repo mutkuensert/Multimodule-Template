8cc58b14eda6adb4813914387e7aa2923f08c9c7
M. Utku Ensert <mutkuensert@hotmail.com>
2025-03-19 18:51:04 +0300

    feat: remove unnecessary configuration in base-presentation

--- buildSrc/src/main/kotlin/base-presentation.gradle.kts
+++ buildSrc/src/main/kotlin/base-presentation.gradle.kts
@@ -1,40 +1,16 @@
 plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
+    id("base-library")
     id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
-    compileSdk = ProjectConfigs.compileSdk
-
-    defaultConfig {
-        minSdk = ProjectConfigs.minSdk
-
-        testInstrumentationRunner = ProjectConfigs.testInstrumentationRunner
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
     buildFeatures {
         compose = true
-        aidl = false
-        buildConfig = true
-        renderScript = false
-        shaders = false
-    }
-
-    compileOptions {
-        sourceCompatibility = ProjectConfigs.sourceCompatibility
-        targetCompatibility = ProjectConfigs.targetCompatibility
-    }
-
-    kotlinOptions {
-        jvmTarget = ProjectConfigs.jvmTarget
     }
 }
 
 dependencies {
     implementation(project(":core:ui"))
-    base()
     baseAndroid()
     compose()
 }
